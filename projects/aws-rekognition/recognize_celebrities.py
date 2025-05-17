bb59bd054c0e0d847c1338b6729826211e491b54
Luiz Antonio <zinfo.luiz@gmail.com>
2025-05-17 16:33:20 -0300

    aws-rekognition -> celebridades

--- projects/aws-rekognition/recognize_celebrities.py
+++ projects/aws-rekognition/recognize_celebrities.py
@@ -7,36 +7,13 @@ client = boto3.client("rekognition")
 def get_path(file_name : str):
     return str(Path(__file__).parent / "imagens" / file_name)
 
-def compare_faces_from_bucket(source_image, target_image, similarity_threshold=80):
-    response = client.compare_faces(
-        SourceImage={
-            "S3Object": {
-                "Bucket": source_image["bucket"],
-                "Name": source_image["name"]
-            }
-        },
-        TargetImage={
-            "S3Object": {
-                "Bucket": target_image["bucket"],
-                "Name": target_image["name"]
-            }
-        },
-        SimilarityThreshold=similarity_threshold
-    )
-    return response
-
-def compare_faces_from_path(source_image, target_image, similarity_threshold=80):
-    with open(source_image, "rb") as source_image_file, open(target_image, "rb") as target_image_file:
-        source_image_bytes = source_image_file.read()
-        target_image_bytes = target_image_file.read()
-    response = client.compare_faces(
-        SourceImage={
-            "Bytes": source_image_bytes
-        },
-        TargetImage={
-            "Bytes": target_image_bytes
-        },
-        SimilarityThreshold=similarity_threshold
+def recognize_celebrities(image_path):
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+    response = client.recognize_celebrities(
+        Image={
+            "Bytes": image_bytes
+        }
     )
     return response
 
@@ -52,23 +29,22 @@ def draw_boxes(image_path,output_path, face_details) -> None:
         
         draw.rectangle([left, top, left + width, top + height], outline="red", width=5)
 
-        similarity = face["Similarity"]
-        draw.text((left, top - 20), f"{similarity:.1f}%", fill="red")
+        draw.text((left, top - 20), f"Name: {face['Name']}, Confidence: {face['MatchConfidence']:.2f}%", fill="red")
 
     image.save(output_path)
     print(f"Image saved to {output_path}")
 
 def main():
-    output_path = get_path("output.jpg")
-    result = compare_faces_from_path(get_path("luiz.jpg"), get_path("foto1.jpg"))
-    if(result["FaceMatches"]):
-        print("Faces match!")
-        for face in result["FaceMatches"]:
-            print(f"Similarity: {face['Similarity']}")
+    output_path = get_path("output2.png")
+    result = recognize_celebrities(get_path("emicida.png"))
+    if(result["CelebrityFaces"]):
+        print("Celebrities found:")
+        for face in result["CelebrityFaces"]:
+            print(f"Name: {face['Name']}, Confidence: {face['MatchConfidence']:.2f}%")
 
-        draw_boxes(get_path("foto1.jpg"), output_path, result["FaceMatches"])
+        draw_boxes(get_path("emicida.png"), output_path, result["CelebrityFaces"])
     else:
-        print("Faces do not match.")
+        print("No celebrities found.")
         #print(result)
 
 if __name__ == "__main__":
